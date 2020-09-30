aa4dfbfb8ebd7fa894229bab2f27cc4b9d59da38
Krisnandika <krisnandikaaji@gmail.com>
2020-09-30 21:01:17 +0700

    Initial setup

---
+++ StatefulDataView/StatefulDataView.h
@@ -0,0 +1,19 @@
+//
+//  StatefulDataView.h
+//  StatefulDataView
+//
+//  Created by Krisnandika Aji on 30/09/20.
+//  Copyright © 2020 Sinar Nirmata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StatefulDataView.
+FOUNDATION_EXPORT double StatefulDataViewVersionNumber;
+
+//! Project version string for StatefulDataView.
+FOUNDATION_EXPORT const unsigned char StatefulDataViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StatefulDataView/PublicHeader.h>
+
+
---
+++ StatefulDataViewExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StatefulDataViewExample
+//
+//  Created by Krisnandika Aji on 30/09/20.
+//  Copyright © 2020 Sinar Nirmata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
